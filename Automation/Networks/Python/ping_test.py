65b8028ed16a7b028dacb301caa3619caea4c35e
Talu <talu.whitley@gmail.com>
2024-09-03 22:42:37 -0600

    Uploading work to GIT first time

--- Automation/Networks/Python/ping_test.py
+++ Automation/Networks/Python/ping_test.py
@@ -2,10 +2,7 @@ import subprocess
 
 def ping_device(host):
     try:
-        # Ping the host once and capture the output
         output = subprocess.check_output(["ping", "-c", "1", host], universal_newlines=True)
-        
-        # Check if the ping was successful
         if "1 packets transmitted, 1 received" in output:
             return True
         else:
@@ -14,19 +11,11 @@ def ping_device(host):
         return False
 
 def main():
-    # Replace '192.168.1.1' with the IP address or hostname you want to ping
-    host = "8.8.8.8"
-    
-    # Ping the device
+    host = input(f"Input IP here: ")
     if ping_device(host):
-        print(f"Ping to {host} was successful!")
-        # Do something on success
-        # For example, continue with the script or execute some logic
-    else:
-        print(f"Ping to {host} failed.")
-        # Do something on failure
-        # For example, retry, alert the user, or exit
+        print(f"IP {host} is taken. Please choose another")
+    else: 
+        print(f"IP {host} is avaiable.")
 
 if __name__ == "__main__":
     main()
-
