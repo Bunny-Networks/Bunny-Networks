c0c3f338ee5295510b42acc739de01e306259f44
Talu <talu.whitley@gmail.com>
2024-09-20 14:22:12 -0600

    Better

--- Hello Arcala/start_here.py
+++
@@ -1,6 +0,0 @@
-user_input = user_input(f'Here is an input: ')
-
-
-if user_input == "1":
-   import end_here
-   end_here.func1
---
+++ Hello_Arcala/start_here.py
@@ -0,0 +1,12 @@
+
+
+
+
+user_input = input(f'Here is an input: ')
+
+
+if user_input == "1":
+   import end_here
+   end_here.func1
+
+
