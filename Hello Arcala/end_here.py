4069058aad9ffa04baf3f53c9dc00705afe9199c
Talu <talu.whitley@gmail.com>
2024-09-20 13:57:57 -0600

    rcola question

---
+++ Hello Arcala/end_here.py
@@ -0,0 +1,13 @@
+def func1():
+      input1 = input(f'ImpoortantInfo: ')
+      input2 = input (f'Im also important!: ')
+
+      being_ping(input1)
+
+def begin_ping(input1)
+
+    ip = input1
+
+def update_whatever()
+
+    vrf = input2 ###
---
+++ Hello Arcala/start_here.py
@@ -0,0 +1,6 @@
+user_input = user_input(f'Here is an input: ')
+
+
+if user_input == "1":
+   import end_here
+   end_here.func1
